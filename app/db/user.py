96e0ad25f1acfdfadc31db01726402baf1526c03
fodro <feodor.tomilov@yandex.ru>
2021-06-22 10:54:42 +0300

    feat: implemented about creation

--- app/db/user.py
+++ app/db/user.py
@@ -1,5 +1,6 @@
-from typing import Union
-from app.db.db_setup import db, User, Salt
+from typing import Union, list, dict
+from datetime import datetime, time
+from app.db.db_setup import db, User, Salt, About, Achievement, Timetable
 import hashlib
 import bcrypt
 
@@ -20,3 +21,25 @@ async def get_user(user_identificator: Union[int, str]) -> User:
 	else:
 		user = db.query(User).filter(User.id == user_identificator).one()
 		return user
+
+async def create_about(user_identificator: Union[int, str],
+							name: str, surname: str, city: str, birthday: datetime, gender: str,
+							citizenships: list(str), rank: str, salary: int, currency: str, stack: list(str),
+							school: str, grade: int, native_language: str, foreign_languages: list(str), can_move: str,
+							metro_station: str, github_id: str, vk_id: str, telegram_id: str, 
+							timetable: list(dict), achievements: list(dict)) -> None:
+	user = get_user(user_identificator)
+	new_about = About(name=name, surname=surname, city=city, birthday=birthday, gender=gender, citizenships=citizenships, rank=rank, 
+		salary=salary, currency=currency, stack=stack, school=school, grade=grade, native_language=native_language,
+		foreign_languages=foreign_languages, can_move=can_move, metro_station=metro_station, github_id=github_id, vk_id=vk_id,
+		telegram_id=telegram_id)
+	for item in timetable:
+		new_time_record = Timetable(day=item["day"], time=item["time"])
+		new_about.timetable.append(new_time_record)
+	for achievement in achievements:
+		new_achievement = Achievement(type=achievement["type"], title=achievement["title"],
+			level=achievement["level"], role=achievement["role"], file=achievement["file"],
+			description=achievement["description"])
+		new_about.achievements.append(new_time_record)
+	db.add(new_about)
+	db.commit()
