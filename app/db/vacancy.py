b3060c8e8422d88ed2331c912578462e58af2ee0
fodro <feodor.tomilov@yandex.ru>
2021-06-23 15:27:32 +0300

    feat: implemented vacancy creation

--- app/db/vacancy.py
+++ app/db/vacancy.py
@@ -1,11 +1,34 @@
-from typing import List
-from app.db.db_setup import Vacancy
+from typing import List, Union, Optional
+from app.db.db_setup import Vacancy, db
+from app.db.user import get_user
 
 async def create_vacancy(
+	user_identifier: Union[int, str],
 	title: str,
-	code: str,
+	vacancy_code: str,
 	description: str,
 	stack: List[str],
+	salary: int, 
+	currency: str,
+	city: str,
+	address: str,
+	type_of_vacancy: str,
+	author: str,
+	phone: Optional[str],
+	email: Optional[str],
+) -> None:
+	user = await get_user(user_identifier)
+	firm = user.firm[0]
+	if phone == None:
+		phone = firm.phone
+	if email == None:
+		email = firm.email
+	new_vacancy = Vacancy(title=title, vacancy_code=vacancy_code,
+		description=description, stack=stack,
+		salary=salary, currency=currency,
+		city=city, address=address,
+		type_of_vacancy=type_of_vacancy, author=author,
+		phone=phone, email=email)
+	firm.vacancies.append(new_vacancy)
+	db.commit()
 	
-):
-	pass
