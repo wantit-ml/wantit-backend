6bd31ee14c030513d82e3aa56e0d4eb6f3302caa
fodro <feodor.tomilov@yandex.ru>
2021-06-23 17:29:56 +0300

    feat: created func to get matching users for HR

--- app/db/user.py
+++ app/db/user.py
@@ -1,8 +1,9 @@
 from typing import Union, List, Dict, Optional
 from datetime import datetime
-from app.db.db_setup import db, User, Salt, About, Achievement, Timetable, Session, Tech, Language
+from app.db.db_setup import Vacancy, db, User, Salt, About, Achievement, Timetable, Session, Tech, Language
 from app.db.tags import get_techs, get_languages
 from app.ml.converter import Converter
+from app.ml.match import MatchForHR
 import hashlib
 import bcrypt
 import uuid
@@ -115,3 +116,16 @@ async def convert_about(user_identifier: Union[int, str]) -> None:
 	code = await converter.convert(about)
 	about.code = code
 	db.commit()
+
+async def get_matching_users(vacancy_id: int) -> List(User):
+	users = db.query(User).all()
+	users_list = []
+	for user in users:
+		users_list.append(user.about[0])
+	vacancy = db.query(Vacancy).filter(Vacancy.id == vacancy_id).one()
+	matching_users_ids = MatchForHR.search_users(vacancy, users)
+	matching_users = []
+	for user in users:
+		if user.id in matching_users_ids:
+			matching_users.append(user)
+	return matching_users
