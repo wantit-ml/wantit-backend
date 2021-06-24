21bd1b8650561ae64407d3a7f74604214d77f194
fodro <feodor.tomilov@yandex.ru>
2021-06-24 14:02:08 +0300

    feat: added matching vacancies fetching

--- app/matching_api/main.py
+++ app/matching_api/main.py
@@ -1,7 +1,9 @@
+from typing import Union
 from fastapi.routing import APIRouter
 from fastapi import Response, responses, status
 from json import dumps
 from app.db.user import get_matching_users
+from app.db.vacancy import get_matching_vacancies
 
 router = APIRouter()
 
@@ -13,4 +15,14 @@ async def fetch_matching_users(vacancy_id: int):
 	matching_users_raw = await get_matching_users(vacancy_id)
 	matching_users = list(map(lambda user: user.id, matching_users_raw))
 	response = dumps(matching_users)
+	return Response(content=response, media_type="application/json", status_code=200)
+
+@router.get(
+	"/get_matching_vacancies",
+	description="This method returns matching vacancies for user. Accepts user indentifier",
+)
+async def fetch_matching_vacancies(user_identifier: Union[int, str]):
+	matching_vacancies_raw = await get_matching_vacancies(user_identifier)
+	matching_vacancies = list(map(lambda vacancy: vacancy.id, matching_vacancies_raw))
+	response = dumps(matching_vacancies)
 	return Response(content=response, media_type="application/json", status_code=200)
