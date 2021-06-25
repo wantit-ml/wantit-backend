79e7613802529cdee0104c2b2f438c68a9df8c85
fodro <feodor.tomilov@yandex.ru>
2021-06-25 10:50:40 +0300

    fixed-indentation

--- app/db/firm.py
+++ app/db/firm.py
@@ -3,26 +3,26 @@ from app.db.db_setup import db, User, Firm
 from app.db.user import get_user
 
 async def create_firm(
-	user_identifier: Union[int, str],
-	title: str,
-	phone: str,
-	email: str,
-	description: str,
-	city: str,
-	address: str
+    user_identifier: Union[int, str],
+    title: str,
+    phone: str,
+    email: str,
+    description: str,
+    city: str,
+    address: str
 ) -> None:
-	user = await get_user(user_identifier)
-	new_firm = Firm(title=title, phone=phone,
-		email=email, description=description,
-		city=city, address=address)
-	user.firm.append(new_firm)
-	db.commit()
+    user = await get_user(user_identifier)
+    new_firm = Firm(title=title, phone=phone,
+        email=email, description=description,
+        city=city, address=address)
+    user.firm.append(new_firm)
+    db.commit()
 
 async def get_firm_by_user_id(user_identifier: Union[int, str]) -> Firm:
-	user = await get_user(user_identifier)
-	return user.firm[0]
+    user = await get_user(user_identifier)
+    return user.firm[0]
 
 async def get_firm_by_id(firm_id: int) -> Firm:
-	firm = db.query(Firm).filter(Firm.id == firm_id).one()
-	return firm
+    firm = db.query(Firm).filter(Firm.id == firm_id).one()
+    return firm
 
--- app/db/user.py
+++ app/db/user.py
@@ -1,16 +1,16 @@
 from typing import Union, List, Dict, Optional
 from datetime import datetime
 from app.db.db_setup import (
-	Vacancy,
-	db,
-	User,
-	Salt,
-	About,
-	Achievement,
-	Timetable,
-	Session,
-	Tech,
-	Language,
+    Vacancy,
+    db,
+    User,
+    Salt,
+    About,
+    Achievement,
+    Timetable,
+    Session,
+    Tech,
+    Language,
 )
 from app.db.tags import get_techs, get_languages
 from app.ml.converter import Converter
@@ -21,186 +21,186 @@ import uuid
 
 
 class UserAlreadyExists(Exception):
-	...
+    ...
 
 
 class WrongPassword(Exception):
-	...
+    ...
 
 
 async def create_user(
-	username: str, password_raw: str, email: str, phone: str, role: str
+    username: str, password_raw: str, email: str, phone: str, role: str
 ) -> None:
-	try:
-		await get_user(username)
-	except:
-		salt = bcrypt.gensalt()
-		hash_object = hashlib.sha256(password_raw.encode() + salt)
-		password = hash_object.hexdigest()
-		new_salt = Salt(salt=salt)
-		new_user = User(
-			username=username, password=password, email=email, phone=phone, role=role
-		)
-		new_user.salt.append(new_salt)
-		db.add(new_user)
-		db.commit()
-	else:
-		raise UserAlreadyExists
+    try:
+        await get_user(username)
+    except:
+        salt = bcrypt.gensalt()
+        hash_object = hashlib.sha256(password_raw.encode() + salt)
+        password = hash_object.hexdigest()
+        new_salt = Salt(salt=salt)
+        new_user = User(
+            username=username, password=password, email=email, phone=phone, role=role
+        )
+        new_user.salt.append(new_salt)
+        db.add(new_user)
+        db.commit()
+    else:
+        raise UserAlreadyExists
 
 
 async def get_user(user_identifier: Union[int, str]) -> User:
-	if type(user_identifier) is str:
-		user = db.query(User).filter(User.username == user_identifier).one()
-		return user
-	else:
-		user = db.query(User).filter(User.id == user_identifier).one()
-		return user
+    if type(user_identifier) is str:
+        user = db.query(User).filter(User.username == user_identifier).one()
+        return user
+    else:
+        user = db.query(User).filter(User.id == user_identifier).one()
+        return user
 
 
 async def create_about(
-	user_identifier: Union[int, str],
-	name: str,
-	surname: str,
-	city: str,
-	birthday: datetime,
-	gender: str,
-	citizenships: List[str],
-	rank: str,
-	salary: int,
-	currency: str,
-	stack: List[str],
-	school: str,
-	age: int,
-	native_language: str,
-	foreign_languages: List[str],
-	can_move: str,
-	metro_station: str,
-	github_id: str,
-	vk_id: str,
-	telegram_id: str,
-	timetable: List[Dict],
-	achievements: List[Dict],
+    user_identifier: Union[int, str],
+    name: str,
+    surname: str,
+    city: str,
+    birthday: datetime,
+    gender: str,
+    citizenships: List[str],
+    rank: str,
+    salary: int,
+    currency: str,
+    stack: List[str],
+    school: str,
+    age: int,
+    native_language: str,
+    foreign_languages: List[str],
+    can_move: str,
+    metro_station: str,
+    github_id: str,
+    vk_id: str,
+    telegram_id: str,
+    timetable: List[Dict],
+    achievements: List[Dict],
 ) -> None:
-	user = await get_user(user_identifier)
-	new_about = About(
-		name=name,
-		surname=surname,
-		city=city,
-		birthday=birthday,
-		gender=gender,
-		citizenships=citizenships,
-		rank=rank,
-		salary=salary,
-		currency=currency,
-		stack=stack,
-		school=school,
-		age=age,
-		native_language=native_language,
-		foreign_languages=foreign_languages,
-		can_move=can_move,
-		metro_station=metro_station,
-		github_id=github_id,
-		vk_id=vk_id,
-		telegram_id=telegram_id,
-	)
-	for item in timetable:
-		new_time_record = Timetable(day=item["day"], time=item["time"])
-		new_about.timetable.append(new_time_record)
-	for achievement in achievements:
-		new_achievement = Achievement(
-			type=achievement["type"],
-			title=achievement["title"],
-			level=achievement["level"],
-			role=achievement["role"],
-			file=achievement["file"],
-			description=achievement["description"],
-		)
-		new_about.achievements.append(new_achievement)
-	user.about.append(new_about)
-	for tech in stack:
-		try:
-			new_tech = Tech(title=tech)
-			db.add(new_tech)
-		except:
-			continue
-	for language in foreign_languages:
-		try:
-			new_language = Language(title=language)
-			db.add(new_language)
-		except:
-			continue
-	db.commit()
-	await convert_about(user_identifier)
+    user = await get_user(user_identifier)
+    new_about = About(
+        name=name,
+        surname=surname,
+        city=city,
+        birthday=birthday,
+        gender=gender,
+        citizenships=citizenships,
+        rank=rank,
+        salary=salary,
+        currency=currency,
+        stack=stack,
+        school=school,
+        age=age,
+        native_language=native_language,
+        foreign_languages=foreign_languages,
+        can_move=can_move,
+        metro_station=metro_station,
+        github_id=github_id,
+        vk_id=vk_id,
+        telegram_id=telegram_id,
+    )
+    for item in timetable:
+        new_time_record = Timetable(day=item["day"], time=item["time"])
+        new_about.timetable.append(new_time_record)
+    for achievement in achievements:
+        new_achievement = Achievement(
+            type=achievement["type"],
+            title=achievement["title"],
+            level=achievement["level"],
+            role=achievement["role"],
+            file=achievement["file"],
+            description=achievement["description"],
+        )
+        new_about.achievements.append(new_achievement)
+    user.about.append(new_about)
+    for tech in stack:
+        try:
+            new_tech = Tech(title=tech)
+            db.add(new_tech)
+        except:
+            continue
+    for language in foreign_languages:
+        try:
+            new_language = Language(title=language)
+            db.add(new_language)
+        except:
+            continue
+    db.commit()
+    await convert_about(user_identifier)
 
 
 async def get_about(user_identifier: Union[int, str]) -> About:
-	user = await get_user(user_identifier)
-	return user.about[0]
+    user = await get_user(user_identifier)
+    return user.about[0]
 
 
 async def create_session(user_identifier: Union[int, str], password_raw: str) -> str:
-	user = await get_user(user_identifier)
-	salt = user.salt[0]
-	hash_object = hashlib.sha256(password_raw.encode() + salt)
-	password = hash_object.hexdigest()
-	if user.password == password:
-		session_id = uuid.uuid1()
-		new_session = Session(session_id=session_id)
-		user.sessions.append(new_session)
-		db.commit()
-		return session_id
-	else:
-		raise WrongPassword
+    user = await get_user(user_identifier)
+    salt = user.salt[0]
+    hash_object = hashlib.sha256(password_raw.encode() + salt)
+    password = hash_object.hexdigest()
+    if user.password == password:
+        session_id = uuid.uuid1()
+        new_session = Session(session_id=session_id)
+        user.sessions.append(new_session)
+        db.commit()
+        return session_id
+    else:
+        raise WrongPassword
 
 
 async def expire_session(session_id: str) -> None:
-	db.query(Session).filter(Session.session_id == session_id).delete(
-		synchronize_session="fetch"
-	)
-	db.commit()
+    db.query(Session).filter(Session.session_id == session_id).delete(
+        synchronize_session="fetch"
+    )
+    db.commit()
 
 
 async def verify_session(user_identifier: Union[int, str], session_id: str) -> bool:
-	user = await get_user(user_identifier)
-	is_valid = False
-	for session in user.sessions:
-		if session.session_id == session_id:
-			is_valid = True
-			break
-	return is_valid
+    user = await get_user(user_identifier)
+    is_valid = False
+    for session in user.sessions:
+        if session.session_id == session_id:
+            is_valid = True
+            break
+    return is_valid
 
 
 async def verify_role(user_identifier: Union[int, str], role) -> bool:
-	user = await get_user(user_identifier)
-	return user.role == role
+    user = await get_user(user_identifier)
+    return user.role == role
 
 
 async def convert_about(user_identifier: Union[int, str]) -> None:
-	about = await get_about(user_identifier)
-	techs = await get_techs()
-	languages = await get_languages()
-	converter = Converter(techs, languages)
-	code = await converter.convert(about)
-	about.code = code
-	db.commit()
+    about = await get_about(user_identifier)
+    techs = await get_techs()
+    languages = await get_languages()
+    converter = Converter(techs, languages)
+    code = await converter.convert(about)
+    about.code = code
+    db.commit()
 
 
 async def get_matching_users(vacancy_id: int) -> List[User]:
-	from app.db.vacancy import get_vacancy_by_id
+    from app.db.vacancy import get_vacancy_by_id
 
-	users = db.query(User).all()
-	users_list = []
-	for user in users:
-		users_list.append(user.about[0])
-	vacancy = await get_vacancy_by_id(vacancy_id)
-	matching_users_ids = await MatchForHR.search_users(vacancy, users_list)
-	matching_users = []
-	for user in users:
-		if user.id in matching_users_ids:
-			matching_users.append(user)
-	return matching_users
+    users = db.query(User).all()
+    users_list = []
+    for user in users:
+        users_list.append(user.about[0])
+    vacancy = await get_vacancy_by_id(vacancy_id)
+    matching_users_ids = await MatchForHR.search_users(vacancy, users_list)
+    matching_users = []
+    for user in users:
+        if user.id in matching_users_ids:
+            matching_users.append(user)
+    return matching_users
 
 async def get_role_by_session_id(session_id: str):
-	session = db.Query(Session).filter(Session.session_id == session_id).one()
-	user = await get_user(session.user_id)
-	return user.role
+    session = db.Query(Session).filter(Session.session_id == session_id).one()
+    user = await get_user(session.user_id)
+    return user.role
