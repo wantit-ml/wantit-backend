f53a0cb882f3e388aeff8b6ada3718ee36845b91
fodro <feodor.tomilov@yandex.ru>
2021-06-28 10:10:17 +0300

    update-ml

--- app/ml/match.py
+++ app/ml/match.py
@@ -1,6 +1,15 @@
 from typing import List
 from app.db.db_setup import About, Vacancy
 
+
+async def count_ones(x):
+    result = 0
+    while x > 0:
+        result += x & 1
+        x = x >> 1
+    return result
+    
+
 """ 
 should be called as 
  users_for_vacancy = await MatchForHR.search_users(vacancy, list_of_about_objects)
@@ -11,6 +20,7 @@ class MatchForHR():
     @classmethod
     async def search_users(cls, vacancy: Vacancy, users_list: List[About]) -> List[int]:
         matched_users_ids = []
+        skills_needed = vacancy.code.count("1")
         for candidate in users_list:
             vacancy_code_len = len(vacancy.code)
             candidate_code_len = len(candidate.code)
@@ -22,7 +32,7 @@ class MatchForHR():
             else:
                 vacancy_code <<= (candidate_code_len - vacancy_code_len)
 
-            if vacancy_code & candidate_code > 0:
+            if count_ones(vacancy_code & candidate_code)/skills_needed >= 0.6:
                 matched_users_ids.append(candidate.id)
         return matched_users_ids
 
@@ -43,6 +53,8 @@ class MatchForUser():
             sample_vacancy_code = int(sample_vacancy.code, 2)
             user_info_code = int(user_info.code, 2)
 
+            skills_needed = sample_vacancy.code.count("1")
+
             if sample_vacancy_code_len > user_info_code_len:
                 user_info_code <<= (
                     sample_vacancy_code_len - user_info_code_len)
@@ -50,6 +62,6 @@ class MatchForUser():
                 sample_vacancy_code <<= (
                     user_info_code_len - sample_vacancy_code_len)
 
-            if sample_vacancy_code & user_info_code >0:
+            if count_ones(sample_vacancy_code & user_info_code)/skills_needed >= 0.6:
                 matched_vacancies_ids.append(sample_vacancy.id)
         return matched_vacancies_ids
